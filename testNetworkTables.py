c96bc268c80fa08da401b9fc6adeee1730e4cd6c
Tanmayi Dasari <tanmayi.dasari@gmail.com>
2023-05-30 18:34:17 -0700

    Update testNetworkTables.py

--- testNetworkTables.py
+++ testNetworkTables.py
@@ -1,13 +1,26 @@
-from cscore import CameraServer, VideoSource, UsbCamera, MjpegServer
+#from cscore import CameraServer, VideoSource, UsbCamera, MjpegServer
 from ntcore import NetworkTableInstance, EventFlags
 
-class testNetworkTables:
-    inst = NetworkTableInstance.getDefault()
-    inst.startServer()
+# from visionInput import VisionInput
+inst = NetworkTableInstance.getDefault()
+inst.startServer()
 
-    if __name__ == "__main__":
-        table = inst.getTable("datatable")
-        
-        xPub = table.getDoubleTopic("x").publish()
-        xPub.set(1)
+if __name__ == "__main__":
+    table = inst.getTable("datatable")
+    
+    print("here")
+    xPub = table.getDoubleTopic("x").publish()
+    xPub.set(1)
+
+    xSub = table.getDoubleTopic("x").subscribe(0)
+    print(xSub.get())
+
+    #print(xPub.getTopic().getProperties())
+
+    # visionInput = VisionInput(70, 960, 720)
+    # img = visionInput.get_frame()
+    # print(img)
+
+    # network tables system is set up --> problem: datatable connection not working, client is not retrieving data 
+    # todo: need to test uploading multiple zipped files to frcvision (this means including the visionInput class)
 
