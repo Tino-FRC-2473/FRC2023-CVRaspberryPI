97ca019861e5cc57ec693fc220370b59f2975023
gracej.cho06@gmail.com <gracej.cho06@gmail.com>
2023-08-29 18:51:26 -0700

    [CV] [add] alignment and turn speed methods to Alignment class

--- alignment.py
+++ alignment.py
@@ -1,17 +1,22 @@
 from simple_pid import PID
+from target import Target
 
 class Alignment:
   #Method that returns whether the robot is aligned to the overall Pipeline system
   def alignedToPipeline():
+    if Target.get_yaw_degrees() > 4 OR Target.get_yaw_degrees() < -4:
+      return false
+    else:
+      return true
 
   def getTurnSpeed():
-    pid = PID(1, 0.1, 0.05, setpoint=1)
+    pid = PID(1, 0.1, 0.05, setpoint=0) #I'm guessing setpoint is the goal yaw angle (0 degrees)
 
-    v = controlled_system.update(0)
+    v = Target.get_yaw_degrees()
 
     while True:
-    # Compute new output from the PID according to the systems current value
+    # this returns the new motor power
     control = pid(v)
 
     # Feed the PID output to the system and get its current value
-    v = controlled_system.update(control)
+    v = Target.get_yaw_degrees
