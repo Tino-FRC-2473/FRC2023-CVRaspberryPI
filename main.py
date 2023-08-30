4c1518143edfba7ad34d473da64c975330565f81
gracej.cho06@gmail.com <gracej.cho06@gmail.com>
2023-08-29 19:16:18 -0700

    [CV][add] updating network table with data returned by alignment methods

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 import cv2
 import numpy as np
 from detector import Detector
+from networktables import NetworkTables
+from detector import Detector
+from alignment import Alignment
 
+NetworkTables.initialize(server='10.24.73.6')
 
 cam = cv2.VideoCapture(0)
 d = Detector()
+a = Alignment()
+
 while True:
+    datatable = NetworkTables.getTable('datatable')
     ret,frame = cam.read()
     d.detectGameElement(np.asarray(frame), ["CUBE", "CONE"])
+    turnSpeed = a.getTurnSpeed()
+    datatable.putNumber('turn speed', turnSpeed)
+    isAligned = a.alignedToPipeline()
+    datatable.putNumber('aligned', isAligned)
