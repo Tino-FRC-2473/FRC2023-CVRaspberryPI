18d42909f30c5b214be132633dc0739b5e5dceea
gracej.cho06@gmail.com <gracej.cho06@gmail.com>
2023-11-09 18:52:55 -0800

    changed method name

--- tester.py
+++ tester.py
@@ -2,4 +2,4 @@ from calibration import calibration
 
 cal_tester = calibration()
 cal_tester.load_images(r"apriltag_pics/")
-print(cal_tester.calibrate_camera(2.54, 6, 9))
+print(cal_tester.calibrate_chessboard(2.54, 6, 9))
