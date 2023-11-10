936fb91125fae91c640ab3fef226d86a668d4710
nandikakohli <nandikakohli05@gmail.com>
2023-11-09 17:54:30 -0800

    (add) fixed spelling

--- tester.py
+++ tester.py
@@ -1,3 +1,5 @@
+from callibration import callibration 
+
 cal_tester = callibration()
-cal_tester.load_images("C:\Users\nandi\Downloads\arduco")
-print(cal_tester.callibrate_camera(2.54, 6, 9))
+cal_tester.load_images(r"C:\Users\nandi\Downloads\arduco")
+print(cal_tester.calibrate_camera(2.54, 6, 9))
