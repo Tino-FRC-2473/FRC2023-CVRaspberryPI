648106b5a3689d1a246c054a8765bf49c3135a5c
Akshay Anand <95551900+AknA13@users.noreply.github.com>
2023-11-16 20:38:03 -0800

    Integrated april tags with rest of modules

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 from detector import Detector
-from detector import Detector
+from apriltag import AprilTag
 from vision_input import VisionInput
 import time
 import ntcore
@@ -9,6 +9,13 @@ import ntcore
 inst = ntcore.NetworkTableInstance.getDefault()
 inst.startClient4("python")
 inst.setServerTeam(2473)
+
+CALIBRATION_DATA_DIRECTORY = '6x10'
+CHECKERBOARD_DIMENSIONS = (5,9)
+CHECKERBOARD_SIZE_INCHES = 0.625
+tag_module = AprilTag()
+tag_module.calibrate_camera(CALIBRATION_DATA_DIRECTORY, CHECKERBOARD_DIMENSIONS, CHECKERBOARD_SIZE_INCHES)
+
 
 FOV = (50.28, 29.16)
 RES = (320, 240)
@@ -29,6 +36,8 @@ while True:
     cubeD = table.getDoubleTopic("cube_distance").publish()
 
     results = d.detectGameElement(np.asarray(frame), ["CUBE", "CONE"])
+    #POSE ESTIMATION APRIL TAG
+    pose_data = tag_module.estimate_3d_pose(frame)
 
     for type, target in results.items():
         if target is not None:
