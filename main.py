2c346d37ec500418cba4c1925b7a63ea9241112f
Akshay Anand <95551900+AknA13@users.noreply.github.com>
2023-11-19 19:31:13 -0800

    integrated new main loop with nt code

--- main.py
+++ main.py
@@ -21,11 +21,12 @@ FOV = (50.28, 29.16)
 RES = (320, 240)
 CAM_HEIGHT = 0.4
 CAM_ANGLE = -15
-d = Detector()
+color_data = {'CUBE': [[110, 100, 100], [158, 255, 255]], 'CONE': [[18.060000000000002, 161.43, 168.63], [31.26, 294.83, 258.07]]}
+d = Detector(color_data)
 input = VisionInput(FOV, RES, CAM_HEIGHT, CAM_ANGLE)
 while True:
-
     frame = input.getFrame()
+    annotated_frame = frame.copy()
     table = inst.getTable("datatable")
     xPub = table.getDoubleTopic("fps_incremented_value").publish()
     xPub.set(frame.sum())
@@ -35,22 +36,24 @@ while True:
     cubeY = table.getDoubleTopic("cube_yaw").publish()
     cubeD = table.getDoubleTopic("cube_distance").publish()
 
-    results = d.detectGameElement(np.asarray(frame), ["CUBE", "CONE"])
-    #POSE ESTIMATION APRIL TAG
-    pose_data = tag_module.estimate_3d_pose(frame)
-
-    for type, target in results.items():
-        if target is not None:
-            yaw = target.get_yaw_degrees()
-            distance = target.get_distance_meters()
-
-            print("detection: ", time.time() - curr)
-            curr = time.time()
-
-            if target.getType() == "CONE":
-                coneY.set(yaw)
-                coneD.set(distance)
-            elif target.getType() == "CUBE":
-                cubeY.set(yaw)
-                cubeD.set(distance)
+    pose_data = tag_module.estimate_3d_pose(frame, annotated_frame)
+    colored_objects = d.detectGameElement(frame, annotated_frame)
+    print(pose_data)
+    for target in colored_objects:
+        type = target.getType()
+        yaw = target.get_yaw_degrees()
+        distance = target.get_distance_meters()
+        if type == "CONE":
+            coneY.set(yaw)
+            coneD.set(distance)
+        elif type == "CUBE":
+            cubeY.set(yaw)
+            cubeD.set(distance)
+        print(type)
+        print(yaw)
+        print(distance)
+    cv2.imshow('result', annotated_frame)
+    key = cv2.waitKey(1) & 0xFF
+    if key == ord('q'):
+        break
     time.sleep(0.02)
